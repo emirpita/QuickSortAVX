61f5f57e53291556b64e30987cb47777a3f24d7a
SamraMujcinovic <smujcinovi1@etf.unsa.ba>
2021-01-11 18:36:55 +0100

    dodano za int8 i double

--- avx2-quicksort.cpp
+++ avx2-quicksort.cpp
@@ -57,7 +57,7 @@ namespace qs {
             int j = right;
 
             const uint16_t pivot = array[(i + j) / 2];
-            const int AVX2_REGISTER_SIZE = 8; // in 32-bit words
+            const int AVX2_REGISTER_SIZE = 16; // in 16-bit words
 
             if (j - i >= 2 * AVX2_REGISTER_SIZE) {
                 qs::avx2::partition_epi16(array, pivot, i, j);
@@ -79,7 +79,7 @@ namespace qs {
             int j = right;
 
             const uint64_t pivot = array[(i + j) / 2];
-            const int AVX2_REGISTER_SIZE = 8; // in 32-bit words
+            const int AVX2_REGISTER_SIZE = 4; // in 64-bit words
 
             if (j - i >= 2 * AVX2_REGISTER_SIZE) {
                 qs::avx2::partition_epi64(array, pivot, i, j);
@@ -93,6 +93,51 @@ namespace qs {
 
             if (i < right) {
                 quicksort_64(array, i, right);
+            }
+        }
+
+        void quicksort_8( uint8_t *array, int left, int right) {
+
+            int i = left;
+            int j = right;
+
+            const uint8_t pivot = array[(i + j) / 2];
+            const int AVX2_REGISTER_SIZE = 32; // in 8-bit words
+
+            if (j - i >= 2 * AVX2_REGISTER_SIZE) {
+                qs::avx2::partition_epi8(array, pivot, i, j);
+            } else {
+                scalar_partition_8(array, pivot, i, j);
+            }
+
+            if (left < j) {
+                quicksort_8(array, left, j);
+            }
+
+            if (i < right) {
+                quicksort_8(array, i, right);
+            }
+        }
+        void quicksort_pd(double *array, int left, int right) {
+
+            int i = left;
+            int j = right;
+
+            const double pivot = array[(i + j) / 2];
+            const int AVX2_REGISTER_SIZE = 4; // in 64-bit words
+
+            if (j - i >= 2 * AVX2_REGISTER_SIZE) {
+                qs::avx2::partition_pd(array, pivot, i, j);
+            } else {
+                scalar_partition_pd(array, pivot, i, j);
+            }
+
+            if (left < j) {
+                quicksort_pd(array, left, j);
+            }
+
+            if (i < right) {
+                quicksort_pd(array, i, right);
             }
         }
     } // namespace avx2
--- partition.cpp
+++ partition.cpp
@@ -88,5 +88,51 @@ void scalar_partition_64(uint64_t *array, const uint64_t pivot, int &left, int &
         }
     }
 }
+void scalar_partition_8(uint8_t *array, const uint8_t pivot, int &left, int &right) {
+
+    while (left <= right) {
+
+        while (array[left] < pivot) {
+            left += 1;
+        }
+
+        while (array[right] > pivot) {
+            right -= 1;
+        }
+
+        if (left <= right) {
+            const uint8_t t = array[left];
+            array[left] = array[right];
+            array[right] = t;
+
+            left += 1;
+            right -= 1;
+        }
+    }
+}
+
+
+void scalar_partition_pd(double *array, const double pivot, int &left, int &right) {
+
+    while (left <= right) {
+
+        while (array[left] < pivot) {
+            left += 1;
+        }
+
+        while (array[right] > pivot) {
+            right -= 1;
+        }
+
+        if (left <= right) {
+            const double t = array[left];
+            array[left] = array[right];
+            array[right] = t;
+
+            left += 1;
+            right -= 1;
+        }
+    }
+}
 
 
