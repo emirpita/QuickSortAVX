a7b697e7a1d957b61c696a63c499c142e5c2ce63
SamraMujcinovic <smujcinovi1@etf.unsa.ba>
2021-01-12 23:53:03 +0100

    radi sve hvala Bogu i ovo je konacna verzija(valjda - ako pita ne bude nesto izmisljao) !!!!

--- gather_results.sh
+++ gather_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+./speed_avx2      100 3 asc | tee -a results.txt
 ./speed_avx2    10000 3 asc | tee -a results.txt
 ./speed_avx2   100000 3 asc | tee -a results.txt
 ./speed_avx2  1000000 3 asc | tee -a results.txt
@@ -9,6 +9,7 @@
 ./speed_avx2 10000000 3 asc | tee -a results.txt
 ./speed_avx2 20000000 3 asc | tee -a results.txt
 
+./speed_avx2      100 3 dsc | tee -a results.txt
 ./speed_avx2    10000 3 dsc | tee -a results.txt
 ./speed_avx2   100000 3 dsc | tee -a results.txt
 ./speed_avx2  1000000 3 dsc | tee -a results.txt
@@ -17,12 +18,13 @@
 ./speed_avx2 10000000 3 dsc | tee -a results.txt
 ./speed_avx2 20000000 3 dsc | tee -a results.txt
 
-./speed_avx2    10000 3 rnd | tee -a results.txt
-./speed_avx2   100000 3 rnd | tee -a results.txt
-./speed_avx2  1000000 3 rnd | tee -a results.txt
-./speed_avx2  2000000 3 rnd | tee -a results.txt
-./speed_avx2  5000000 3 rnd | tee -a results.txt
-./speed_avx2 10000000 3 rnd | tee -a results.txt
-./speed_avx2 20000000 3 rnd | tee -a results.txt
+./speed_avx2      100 3 randomuniq | tee -a results.txt
+./speed_avx2    10000 3 randomuniq | tee -a results.txt
+./speed_avx2   100000 3 randomuniq | tee -a results.txt
+./speed_avx2  1000000 3 randomuniq | tee -a results.txt
+./speed_avx2  2000000 3 randomuniq | tee -a results.txt
+./speed_avx2  5000000 3 randomuniq | tee -a results.txt
+./speed_avx2 10000000 3 randomuniq | tee -a results.txt
+./speed_avx2 20000000 3 randomuniq | tee -a results.txt
 
 echo "results.txt created"
